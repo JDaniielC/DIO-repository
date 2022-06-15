4805867d4a7658182e37544fe2a70ec1feca6b45
José Daniel <jdsc2@cin.ufpe.br>
2022-06-15 06:23:53 -0300

    FIX: Descricao and DataNotations

---
+++ AspDotNet/AspDotNet/Migrations/20220615091910_NovaTabelaProduto.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AspDotNet.Migrations
+{
+    public partial class NovaTabelaProduto : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Descricao",
+                table: "Produtos",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descricao",
+                table: "Categorias",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Descricao",
+                table: "Produtos",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descricao",
+                table: "Categorias",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
