b07f8b39b034d885623cc46802df73a1650c7304
José Daniel <jdsc2@cin.ufpe.br>
2022-06-15 05:55:28 -0300

    FEAT: ASP.NET

---
+++ AspDotNet/AspDotNet/Models/Context.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspDotNet.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        // Para ser feito isso precisou instalar o SqlServer e Tools:
+        // https://docs.microsoft.com/pt-br/ef/core/get-started/overview/install
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Cursomvc;Integrated Security=True");
+        }
+    }
+}
