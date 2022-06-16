68a3fde21a5821d94be7fe224b96d91d84e5e2f4
José Daniel <jdsc2@cin.ufpe.br>
2022-06-15 23:14:50 -0300

    TEST: Está com erro na linha 26/34.

--- AspDotNet/AspDotNet/Models/Context.cs
+++ AspDotNet/AspDotNet/Models/Context.cs
@@ -17,5 +17,15 @@ namespace AspDotNet.Models
         {
             optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Cursomvc;Integrated Security=True");
         }
+        // Com esse método novo, foi modificado o controller do Produto em PUT!
+        public virtual void SetModified(object entity)
+        {
+            Entry(entity).State = EntityState.Modified;
+        }
+
+        public static implicit operator global::Moq.Mock<object>(Context v)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
