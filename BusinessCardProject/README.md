8ad74604fca666aec87fbbe6cc35c4037677be86
José Daniel <jdsc2@cin.ufpe.br>
2022-07-01 02:16:49 -0300

    Projeto cartão de visita finalizado

---
+++ BusinessCardProject/README.md
@@ -0,0 +1,7 @@
+# Business Card Project
+
+Projeto em Kotlin de aplicativo para criação e exibição de cartões de visita.
+
+![Logo](./.github/Home.png)
+
+### O projeto contém persistencia de dados.
--- BusinessCardProject/app/src/main/java/com/example/businesscard/ui/MainActivity.kt
+++ BusinessCardProject/app/src/main/java/com/example/businesscard/ui/MainActivity.kt
@@ -1,15 +1,41 @@
 package com.example.businesscard.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import com.example.businesscard.App
 import com.example.businesscard.databinding.ActivityMainBinding
+import com.example.businesscard.ui.adapter.BusinessCardAdapter
 
 class MainActivity : AppCompatActivity() {
     // Toda activity deve ser feito o binding:
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val mainViewModel: MainViewModel by viewModels {
+        MainViewModelFactory((application as App).repository)
+    }
+    private val adapter by lazy { BusinessCardAdapter() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        binding.rcCards.adapter = adapter
+
+        // Functions:
+        getAllBusinessCard()
+        insertListener()
+    }
+
+    private fun insertListener() {
+        binding.fabMain.setOnClickListener {
+            val intent = Intent(this, AddBusinessCardActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun getAllBusinessCard() {
+        mainViewModel.getAll().observe(this, { businessCard ->
+            adapter.submitList(businessCard)
+        })
     }
 }
